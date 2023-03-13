4439c7acf2eefac87b1a1dccda79ce2d326e3b71
kur0dama <hk2557@protonmail.com>
2023-03-12 20:02:42 -0400

    Add error handling to main()

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,19 @@ use data::{get_json_from_file, SpcData, SpcDataRow};
 use temporal::DateMap;
 
 fn main() {
-    let request_json = get_json_from_file(Path::new("data/data.json")).unwrap();
-    let spc_data_opt = SpcData::try_from(&request_json);
+    let request_json_opt = get_json_from_file(Path::new("data/data.json"));
+    if request_json_opt.is_err() {
+        println!("{}", request_json_opt.unwrap_err());
+        return;
+    }
+    let spc_data_opt = SpcData::try_from(&request_json_opt.unwrap());
     match spc_data_opt {
         Ok(spc_data) => {
             let inner_data = &spc_data.data;
             let date_freq = &spc_data.target_date_freq;
             let date_vec = inner_data
                 .iter()
-                .map(|s: &SpcDataRow| s.dt)
+                .map(|s| s.dt)
                 .collect::<Vec<NaiveDateTime>>();
             let min_date = date_vec.iter().min().unwrap();
             let max_date = date_vec.iter().max().unwrap();
@@ -28,7 +32,6 @@ fn main() {
                 println!("{}: {:?}", k, v);
             }
         }
-        // Err(error) => print!("{}", error),
-        Err(error) => println!("{}", error),
+        Err(e) => println!("{}", e),
     }
 }
